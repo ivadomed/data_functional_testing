95f96606c9480005756042d232f1d55d28d76675
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-02-08 09:54:45 -0800

    test

--- .github/learn-github-actions.sh
+++ .github/learn-github-actions.sh
@@ -6,9 +6,8 @@ FILES=$(git diff --name-only --line-prefix=`git rev-parse --show-toplevel`/ HEAD
 echo "${FILES}"
 echo `git branch -r`
 git diff --name-only "$GITHUB_BASE_REF..$GITHUB_SHA"
-# git diff --name-only `git merge-base origin/master ${GITHUB_REF#refs/heads/}`..${GITHUB_REF#refs/heads/}
+git diff --name-only `git merge-base origin/master ${GITHUB_REF#refs/heads/}`...${GITHUB_REF#refs/heads/}
 # git diff --name-only origin/master...${GITHUB_REF#refs/heads/}
-git diff --name-only ${GITHUB_REF#refs/heads/} origin/master
 
 # file_list=($FILES)
 # for i in "${file_list[@]}"
